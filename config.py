da82d915e53e1d8803099b3320e36c950cab4830
lakki99 <pulka143341@gmail.com>
2025-05-11 22:15:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,13 +9,13 @@
 import os
 
 class Config:
-    API_ID = os.environ.get("API_ID", "")
-    API_HASH = os.environ.get("API_HASH", "")
+    API_ID = os.environ.get("API_ID", "22432833")
+    API_HASH = os.environ.get("API_HASH", "897f1c440892cfc46c7e222dfb37d015")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = os.environ.get("BOT_SESSION", "forward-bot") 
-    DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("DB_NAME", "madflixbotz")
-    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '').split()]
+    BOT_SESSION = os.environ.get("BOT_SESSION", "Mitsuri_kanroji_forward_bot") 
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://enobot12345:4wEyfM0FvZxNhAKi@cluster0.a4rj3dj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_NAME = os.environ.get("DB_NAME", "lakki")
+    OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '7592041488').split()]
 
 
 class temp(object): 
