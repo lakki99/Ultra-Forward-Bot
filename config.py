a00159c3190a4fe7a01ffef473f3090a2bf72c82
Lakki reddy <pulka143341@gmail.com>
2025-07-16 17:49:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "d3cd72e44e4160ce1ee0b7947e8f2d89")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "Mitsuri_kanroji_forward_bot") 
-    DB_URL = os.environ.get("DB_URL", "mongodb+srv://enobot12345:4wEyfM0FvZxNhAKi@cluster0.a4rj3dj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_URL = os.environ.get("DB_URL", "")
     DB_NAME = os.environ.get("DB_NAME", "lakki1")
     OWNER_ID = [int(id) for id in os.environ.get("OWNER_ID", '7460363208').split()]
 
